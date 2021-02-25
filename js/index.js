3df0eea923d7dfe8127e745ce46f0c321a11ad11
rbisnath-school <70607506+rbisnath-school@users.noreply.github.com>
2021-02-24 19:32:54 -0500

    fixed the animation and added a button fade in

--- js/index.js
+++ js/index.js
@@ -2,5 +2,10 @@ console.log("Totes malotes dawg... Your index.js file is loaded correctly!");
 
 
 $(function() {
-    $( "p" ).text( "The DOM is now loaded and can be manipulated." );
+  $('#cta').fadeIn("slow");
   });
+
+
+
+
+  
